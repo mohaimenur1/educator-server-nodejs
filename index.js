53ee2ce49b717cb43ea5ac52fd8266c0a29b1728
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-10-25 13:47:27 +0600

    npm init to initialize our package file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const programs = require("./data/programs.json");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api running");
+});
+
+app.get("/programs", (req, res) => {
+  res.send(programs);
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log("port running successfully on " + port);
+});
